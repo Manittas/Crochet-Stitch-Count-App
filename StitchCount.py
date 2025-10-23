2a02c929d9db337148ac0400594fbb8a043d317c
Filipe Neves <66378018+Manittas@users.noreply.github.com>
2025-10-23 22:21:38 +0100

    keyboard button functionalities, input field, reset functionality, add variables class to main class

--- StitchCount.py
+++ StitchCount.py
@@ -1,13 +1,15 @@
+# Only necessary to delete .spec file before pyinstaller command
 # pyinstaller --clean --onefile --noconsole --icon icon.ico your_script.py
+# ------------------------------------------------------------------------
 
 # from pathlib import Path
-from tkinter import Tk, Canvas, Button, font
+from tkinter import Tk, Canvas, Button, font, Entry, END
 from models.Row import Row
 
+# Window properties
+# -----------------
+
 window = Tk()
-
-# Set window properties
-
 window.geometry("300x300")
 window.resizable(False, False)
 window.attributes("-topmost", True)
@@ -23,12 +25,32 @@ canvas = Canvas(
 )
 canvas.pack()
 
-# row object and related
+# Objects and variables
+# ---------------------
+
+class AppVariables:
+    def __init__(self):
+        self.inputVisible = False  # toggles the input field to be visible
+        self.inputWindow = None    # default store value for canvas window ID
 
 row = Row()
-manualCount = 0
+variables = AppVariables()
 
 # functions
+# ----------
+
+def on_key_press(event):
+    # keyboard event system for the functionalities, input can't be visible
+    if not variables.inputVisible:
+        match event.keysym:
+            case "q" | "Q":
+                decrement_button_click()
+            case "e" | "E":
+                increment_button_click()
+            case "r" | "R":
+                reset_count()
+            case _:
+                return
 
 def increment_button_click():
     row.increment()
@@ -40,20 +62,44 @@ def decrement_button_click():
         row.decrement()
         update_count()
 
-def manual_set_count_click():
-    row.count(manualCount)
+def input_set_count():
+    newCount = inputField.get()
+    # verification so it only changes when input is visible and with value
+    if variables.inputVisible and not(newCount == ""):
+        row.count = int(newCount)
+        inputField.delete(0, END)
+        update_count()
+
+def reset_count():
+    row.count = 0
     update_count()
     
 def update_count():
     canvas.itemconfig(rowLabel, text=f"Count: {row.count}")
     
-    # toglles/untoggles decrement button depending on count value update
+    # toggles/untoggles decrement button depending on count value update
     if row.count == 0:
         decrementBtn.config(state="disabled")
     else:
         decrementBtn.config(state="normal")
 
+def toggle_input_field():
+    # deletes any value in the field, visibe or not
+    inputField.delete(0, END)
+    # creates / destroys the input field to be visible
+    if variables.inputVisible:
+        variables.inputVisible = False
+        canvas.delete(variables.inputWindow)
+        variables.inputWindow = None
+    else:
+        variables.inputVisible = True
+        variables.inputWindow = canvas.create_window(150, 210, window=inputField)
+        
+def is_number(char):
+    return char.isdigit() or char == ""
+
 # labels
+# ------
 
 canvas.create_text(150,
                    20,
@@ -73,19 +119,31 @@ rowLabel = canvas.create_text(150,
                               fill="white",
                               font=("Arial", 16, "bold"))
 
-# buttons
+# buttons & Inputs
+# ----------------
+
 btn_font = font.Font(family="Arial", size=10, weight="bold")
+validate_cmd = window.register(is_number)
 
 incrementBtn = Button(window, text="Add", font=btn_font, width=4, height=1, command=increment_button_click)
 decrementBtn = Button(window, text="Sub", state="disabled", font=btn_font, width=4, height=1, command=decrement_button_click)
-manualSetBtn = Button(window, text="Set", font=btn_font, width=4, height=1, command=manual_set_count_click)
+manualSetBtn = Button(window, text="Set", font=btn_font, width=4, height=1, command=toggle_input_field)
+
+inputField = Entry(window, validate="key", validatecommand=(validate_cmd, "%P"))
 
 canvas.create_window(200,
                      175,
                      window=incrementBtn)
+canvas.create_window(150,
+                     175,
+                     window=manualSetBtn)
 canvas.create_window(100,
                      175,
                      window=decrementBtn)
 
-# start app main loop
+# App bindings and main loop
+# --------------------------
+
+window.bind("<KeyPress>", on_key_press)
+window.bind("<Return>", lambda e=None: input_set_count())
 window.mainloop()
