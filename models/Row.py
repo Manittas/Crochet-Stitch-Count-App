55b313c46a24ef9ab6298a731c36eb8e44562503
Filipe Neves <66378018+Manittas@users.noreply.github.com>
2025-10-24 16:37:48 +0100

    add id and current flag to row object

--- models/Row.py
+++ models/Row.py
@@ -1,20 +1,43 @@
+import uuid
+
 class Row:
-    def __init__(self, _count = 0):
+    def __init__(self, _id = uuid.uuid4().hex, _count = 0, _isCurrent = False):
+        self._id = _id
         self._count = _count
+        self._isCurrent = _isCurrent
         
     # class methods
+    # -------------
+    
+    @property
+    def rowId(self):
+        return self._id
     
     @property
     def count(self):
         return self._count
     
+    @property
+    def isCurrent(self):
+        return self._isCurrent
+    
+    
+    @rowId.setter
+    def rowId(self, newId):
+        self._id = newId
+            
     @count.setter
     def count(self, value):
         # prevent setting value to negatives or non int values
         if isinstance(value, int) and value >= 0:
             self._count = value
+    
+    @isCurrent.setter
+    def rowId(self, isCurrent):
+        self._isCurrent = isCurrent
             
     # instance methods
+    # ----------------
     
     def increment(self):
         self._count += 1
